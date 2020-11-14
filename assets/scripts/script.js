6e3527d982706ca0234a3270c6bbeaeddd64950f
Truc Nguyen <trucn0215@gmail.com>
2020-11-14 11:13:22 -0800

    edit the layout on the rule button to click to show and click again to hidden

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,1 +1,9 @@
 // used to run the game
+function showRuleButton() {
+    var x = document.getElementById("game-rule-text");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
